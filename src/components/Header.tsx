7511c0ffd7fa59c5246ec0635682f5bd6c843bea
Zaven477 <Papikyan86@bk.ru>
2025-07-13 02:34:20 +0300

    add active tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import "./Header.css";
 export const Header = () => {
   return (
     <header className="header">
-      <div className="header-content"></div>
+      <div className="header-content">
+        <div className="header-tab">Все котики</div>
+      </div>
     </header>
   );
 };
