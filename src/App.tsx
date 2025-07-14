43346b78a8748354cc482f66764a536da2b86b86
Zaven477 <Papikyan86@bk.ru>
2025-07-14 12:01:54 +0300

    update Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
 
 export const App = () => {
+  
+
   return (
     <div className="app">
       <Header />
