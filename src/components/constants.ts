d70be3b36a69a466865f52bf971b7132c479f2ab
Zaven477 <Papikyan86@bk.ru>
2025-07-15 13:49:32 +0300

    we move the key into a variable

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,2 +1,3 @@
 export const COUNT_IMAGES = 6;
 export const FAVORITES_STORAGE_KEY = "cat-gallery-favorites";
+export const API_KEY = 'live_fHrUNHA96CMXDTbia9Xw4eSfJAovSZ0LWw5NPslMT79piyDM4IsdzPNJz5ktlzO3'
